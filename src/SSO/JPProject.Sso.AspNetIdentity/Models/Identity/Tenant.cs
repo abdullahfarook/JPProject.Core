d4ea9aa89001b49a8585fb5c6da767ffca15a721
Abdullah Farooq <afarooq@brickclay.com>
2020-12-08 21:17:21 +0500

    added multi-tenant views + functionality

---
+++ src/SSO/JPProject.Sso.AspNetIdentity/Models/Identity/Tenant.cs
@@ -0,0 +1,18 @@
+﻿#nullable enable
+using MultiTenancyServer;
+
+namespace JPProject.Sso.AspNetIdentity.Models.Identity
+{
+    public class Tenant : TenancyTenant
+    {
+        protected Tenant(){}
+        public Tenant(string name,string displayName, string? logo = null)
+        {
+            base.CanonicalName = name;
+            DisplayName = displayName;
+            Logo = logo;
+        }
+        public string DisplayName { get; set; } = null!;
+        public string? Logo { get; set; }
+    }
+}
---
+++ src/SSO/JPProject.Sso.AspNetIdentity/Models/Identity/UserRoleIdentity.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace JPProject.Sso.AspNetIdentity.Models.Identity
+{
+    public sealed class UserRoleIdentity : IdentityUserRole<string>
+    {
+        public UserRoleIdentity() { }
+
+        public UserRoleIdentity(Tenant tenant, RoleIdentity role, UserIdentity user)
+        {
+            TenantId = tenant.Id;
+            RoleId = role.Id;
+            UserId = user.Id;
+        }
+        public string TenantId { get; set; }
+    }
+}
